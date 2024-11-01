17b73e51819b07c849fe82bbbebea2b020f27934
iDevorei <messewalter@gmail.com>
2024-11-01 03:01:49 +0300

    finish Homework 3

--- Homework_3/scripts/script.js
+++ Homework_3/scripts/script.js
@@ -211,7 +211,6 @@ numForUser();
 // «4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57». Найдите самое большое и самое маленькое число в строке, используя цикл.
 // *solution
 
-// !хочется найти ещё какое-то решение
 const strMaxMin = () => {
     let str = '4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57';
     let maxNum = -Infinity;
@@ -233,6 +232,32 @@ const strMaxMin = () => {
 };
 
 strMaxMin();
+
+const secondStrMinMax = () => {
+    let str = '4 57 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 98';
+    let maxNum = -Infinity;
+    let minNum = Infinity;
+    let flag = '';
+
+    for(let i = 0; i <= str.length; i++) {
+        if (str[i] !== ' ' && str[i] !== undefined) {
+            flag += str[i];
+        } else {
+            if (+maxNum < +flag) {
+                maxNum = +flag;
+            }
+            if (+minNum > +flag) {
+                minNum = +flag;
+            }
+            flag = '';
+        }
+    }
+
+    console.log(maxNum);
+    console.log(minNum);
+}
+
+secondStrMinMax();
 
 // *task 10
 // Дано произвольное целое число n. Написать программу, которая: 
@@ -466,12 +491,54 @@ const triangle = () => {
 
 triangle();
 
+console.log(`----------------------------------`);
+
+// *task 11 extra
+// Напишите программу, которая проверяет правильность ввода пароля от пользователя. Важно, чтобы пароль соотв. сл. требованиям: длина пароля от 6 символов; пароль должен состоять только из лат. символов; содержать минимум 1 число, 1 большой символ и любой знак из списка: _*&-#$.
+
+const passCheck = () => {
+    // let pass = prompt('Введите пароль: ', 's052_Y');
+    let pass = 's052_Y';
+    let trueStr = '_*&-#$0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+    let trueUpperStr = 'QWERTYUIOPASDFGHJKLZXCVBNM';
+    let passCheck = '';
+
+    for(let i = 0; i < pass.length; i++) {
+        let char = pass[i];
+
+        for(let j = 0; j < trueStr.length; j++) {
+            if (char === trueStr[j]) {
+                passCheck += char;
+            }
+        }
+    }
+
+    let checkUpperLetter = false;
+    for(let i = 0; i < pass.length; i++) {
+        let char = pass[i];
+
+        for(let j = 0; j < trueUpperStr.length; j++) {
+            if (char === trueUpperStr[j]) {
+                checkUpperLetter = true;
+                break;
+            }
+        }
+    }
+
+    if (pass.length >= 6 && passCheck === pass && checkUpperLetter === true) {
+        console.log(`Good pass!`);
+    } else console.log(`Wrong pass!`);
+
+}
+
+passCheck();
+
+console.log(`----------------------------------`);
+
 // *task 12 extra
 // Посчитать, сколько раз встречается определенная цифра в введенной последовательности чисел. Количество вводимых чисел и цифра, которую необходимо посчитать вводится пользователем.
 // *solution
 
-// !этот код жутко тормозит
-// !работает только, если нужно найти однозначное число
 const numIntoNum = () => {
     let numb = +prompt('ВВедите число, в котором будем искать цифру');
     let numbToStr = String(numb);
@@ -486,3 +553,89 @@ const numIntoNum = () => {
 };
 
 numIntoNum();
+
+console.log(`----------------------------------`);
+
+// *task 13 extra
+// Найти число с максимальной суммой цифр среди чисел: 56,987,103,9011,45.
+// *solution
+
+const maxSumNums = () => {
+    let str = '56,987,103,9011,45';
+    let max = -Infinity;
+    let num = '';
+    let maxNum;
+
+    for(let i = 0; i < str.length; i++) {
+        if (str[i] !== undefined && str[i] !== ',') {
+            num += str[i];
+            console.log(num);
+        }
+        else {
+            let sum = 0;
+            for (let j = 0; j < num.length; j++) {
+                sum += +num[j];
+            }
+
+            if (sum > max) {
+                max = sum;
+                maxNum = num;
+            }
+
+            console.log(num, ` = ${sum}`);
+            num = '';
+        }
+    }
+    console.log(`Max num ${maxNum}, ${max}`);
+}
+
+maxSumNums();
+
+console.log(`----------------------------------`);
+
+// *task 14 extra
+// 14. Вывести на экран «прямоугольник», образованный из двух видов символов. Контур прямоугольника должен состоять из одного символа, а в «заливка» — из другого.
+// ##############################
+// #0000000000000000000000000000#
+// #0000000000000000000000000000#
+// #0000000000000000000000000000#
+// #0000000000000000000000000000#
+// ##############################
+
+// 1. Сколько строк? => 6
+// 2. Сколько символов в одной строке? => 30
+// 3. Какой символ рамки? => "#"
+// 4. Какой символ заполнения? => "0"
+// 5. Создаем цикл для создания строк (6шт)
+// 6. Создаем переменную для формирования каждой строки из симоволов длиной 30
+//     6.1 Если первая и последняя строка, то символы в строке должны быть "#"
+//     6.2 В остальных строках: каждая начинается и заканчивает #, а ост. символы 0
+
+const picture = () => {
+    let height = 6;
+    let width = 30;
+    let border = '#';
+    let main = '0';
+    let result = '';
+
+    for (let i = 1; i <= height; i++) {
+        let line = '';
+
+        for (let j = 1; j <= width; j++) {
+            if (i === 1 || i === height) {
+                line += border;
+            } else {
+                if (j === 1 || j === width) {
+                    line += border
+                } else {
+                    line += main;
+                }
+            }
+        }
+        document.write(line + '<br>');
+        result += line + '\n';
+    }
+    console.log(result);
+}
+
+picture();
