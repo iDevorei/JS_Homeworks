8a9d9a48b08b1155976d990a0c78cae6d08d315e
iDevorei <messewalter@gmail.com>
2024-11-04 19:29:02 +0300

    feat: HW 5, tasks 5-7

--- Homework_5/scripts/script.js
+++ Homework_5/scripts/script.js
@@ -72,3 +72,78 @@ function showArr(arr) {
 }
 
 result = showArr(createArr());
+
+// *task 5
+// Сделайте функцию isEven() (even - это четный), которая параметром 
+// принимает целое число и проверяет: четное оно или нет. Если четное - пусть 
+// функция возвращает true, если нечетное — false.
+// *solution
+
+function isEven(number) {
+    return number % 2 === 0;
+}
+
+// *task 6
+// Напишите ф-цию, в которую передается массив с целыми числами. 
+// Верните новый массив, где останутся лежать только четные из этих чисел. 
+// Для этого используйте вспомогательную функцию isEven из предыдущей 
+// задачи.
+// *solution
+
+let someArr = [2, 3, 6, 5, 23, 22, 111, 100, -4, -5];
+function getEvenArr(arr) {
+    let evenArr = [];
+    for(let item of arr) {
+        if (isEven(item)) evenArr.push(item);
+    }
+    return evenArr;
+}
+
+console.log(getEvenArr(someArr));
+
+// *task 7
+// Напишите ф-цию, которая рисует следующую пирамидку (исп. вложенные 
+//     циклы):
+//     1 
+//     22 
+//     333 
+//     4444 
+//     55555 
+//     666666 
+//     7777777 
+//     88888888
+//     999999999
+//     Кол-во рядов должно вводиться параметром. Если пользователь ввел доп. 
+//     параметр, непредусмотренный ф-цией по умолчанию - один любой символ, 
+//     кроме пробела, то пирамида должна быть нарисована этим символом
+// *solution
+
+const getBasisFoTreangle = () => {
+    let number = +prompt('Введите число для основания греугольника');
+    // let number = 9;
+    return number;
+}
+
+const getNonStandartSymbol = () => {
+    let symbol = prompt(`Если хотите, можете ввести не стандартный символ`);
+    // let symbol = '*';
+    if (!symbol) return 1;
+    else return symbol;
+}
+
+const createTreangle = (basis, elem) => {
+    let str = '';
+    let basisLength = basis;
+    for(let i = 0; i < basisLength; i++) {
+        for(let j = 0; j < basisLength; j++) {
+            if (j < (basis - 1)) str += ``;
+            else str += elem;
+        }
+        if (!isNaN(elem)) elem++;
+        basis--;
+        str += `\n`;
+    }
+    return str;
+}
+
+console.log(createTreangle(getBasisFoTreangle(), getNonStandartSymbol()));
