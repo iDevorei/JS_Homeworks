864996ad044647b70e4898dfa2ab0b957572541d
iDevorei <messewalter@gmail.com>
2024-11-14 17:38:10 +0300

    fix: update task 11. Finish Homework 4

--- Homework_4/scripts/script.js
+++ Homework_4/scripts/script.js
@@ -284,7 +284,27 @@ const triangle = () => {
 
 }
 
-triangle();
+// triangle();
+
+// Правильное решение
+
+const rightTriangle = (height, symbol) => {
+    for(let i = 1; i <= height; i++) {
+        let line = '';
+
+        for(let j = 1; j <= (height - i); j++) {
+            line += ' ';
+        }
+
+        for(let k = 1; k <= ((i * 2) - 1); k++) {
+            line += symbol;
+        }
+
+        console.log(line);
+    }
+}
+
+rightTriangle(9, '^');
 
 console.log(`----------------------------------------`);
 
